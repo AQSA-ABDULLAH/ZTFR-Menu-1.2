26c586ab37fc496ebe03343bd0e54a2792a825ac
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2025-01-07 14:24:03 +0500

    fix styling

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -6,13 +6,13 @@ import "slick-carousel/slick/slick-theme.css";
 function Sidebar() {
   const carouselSettings = {
     infinite: true,
-    speed: 2000, // Longer duration for smoother horizontal scrolling
+    speed: 2000,
     slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 0, // Continuous scrolling
+    autoplaySpeed: 0,
     arrows: false,
-    cssEase: "linear", // Smooth scrolling effect
+    cssEase: "linear",
     responsive: [
       {
         breakpoint: 768,
@@ -31,16 +31,16 @@ function Sidebar() {
 
   const verticalCarouselSettings = {
     infinite: true,
-    speed: 2000, 
+    speed: 2000,
     slidesToShow: 3,
     slidesToScroll: 1,
     vertical: true,
     verticalSwiping: true,
     autoplay: true,
-    autoplaySpeed: 0, 
+    autoplaySpeed: 0,
     arrows: false,
-    cssEase: "linear", 
-    rtl: true, 
+    cssEase: "linear",
+    rtl: true,
     responsive: [
       {
         breakpoint: 768,
@@ -58,18 +58,11 @@ function Sidebar() {
   };
 
   return (
-    <div className="bg-black fixed text-white top-0 right-0 h-[100vh] w-[960px] p-4">
+    <div className="bg-black fixed text-white top-0 right-0 h-[100vh] w-[960px] p-5">
       <header className="flex justify-between">
         <div className="flex">
           <img src="/assets/Path 27323.png" alt="close" />
           <p>MENU</p>
-        </div>
-        <div>
-          <img src="/assets/Group 6113.png" alt="logo" />
-        </div>
-        <div className="flex">
-          <img src="/assets/Group 2867.png" alt="user-icon" />
-          <img src="/assets/Group 5511.png" alt="UK-Flag" />
         </div>
       </header>
       <div className="flex flex-row">
@@ -87,10 +80,17 @@ function Sidebar() {
           </Slider>
         </div>
 
-        <div>
-          <div className="flex">
-            <img src="/assets/Group 6421.png" alt="image" />
-            <div className="flex flex-col justify-between">
+        <div className="items-end">
+          <div className="flex justify-between">
+            <div>
+              <img src="/assets/Group 6113.png" alt="logo" />
+              <img src="/assets/Group 6421.png" alt="image" />
+            </div>
+            <div className="flex flex-col justify-between items-end">
+              <div className="flex">
+                <img src="/assets/Group 2867.png" alt="user-icon" />
+                <img src="/assets/Group 5511.png" alt="UK-Flag" />
+              </div>
               <p>FEATURES</p>
               <p>PRODUCTS</p>
               <p>HOW TO ZTFR</p>
@@ -102,14 +102,14 @@ function Sidebar() {
               <p>NEWSROOM/PRESS</p>
             </div>
           </div>
-          <div className="flex">
+          <div className="flex items-center gap-10">
             <img src="/assets/Group 6422.png" alt="image" />
-            <img src="/assets/Group 6420.png" alt="image" />
+            <img src="/assets/Group 6420.png" alt="image" className="h-[10rem]" />
           </div>
         </div>
       </div>
 
-      <div className="">
+      <div className="pt-4">
         <Slider {...carouselSettings}>
           <div>
             <img src="/assets/Group 6408.png" alt="image" />
@@ -136,6 +136,3 @@ function Sidebar() {
 }
 
 export default Sidebar;
-
-
-
