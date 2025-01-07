1b3d85d5dc767e536ee252e4e7999751bf08be9f
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2025-01-07 08:56:34 +0500

    create sidebar

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -2,24 +2,47 @@ import React from "react";
 
 function Sidebar() {
   return (
-    <div className="bg-black fixed top-0 right-0 min-h-screen w-[960px]">
-      <div className="flex flex-row">
-        <div className="flex flex-col w-[12rem]">
+    <div className="bg-black fixed text-white top-0 right-0 min-h-screen w-[960px]">
+      <header className="flex justify-between px-3 pt-3">
+        <div className="flex gap-4">
+          <img src="/assets/Path 27323.png" alt="close" />
+          <p>MENU</p>
+        </div>
+        <div>
+          <img src="/assets/Group 6113.png" alt="logo" />
+        </div>
+        <p>IMAGE</p>
+      </header>
+      <div className="flex flex-row p-3">
+        <div className="flex flex-col space-y-3">
           <img src="/assets/Group 6408.png" alt="image" />
           <img src="/assets/Group 6407.png" alt="image" />
           <img src="/assets/Group 6407.png" alt="image" />
         </div>
 
         <div>
-          <img src="/assets/Group 6421.png" alt="image" />
-          <div className="flex">
-          <img src="/assets/Group 6422.png" alt="image" />
-          <img src="/assets/Group 6420.png" alt="image" />
+          <div className="flex p-3 space-x-3">
+            <img src="/assets/Group 6421.png" alt="image" />
+            <div className="flex flex-col justify-between">
+              <p>FEATURES</p>
+              <p>PRODUCTS</p>
+              <p>HOW TO ZTFR</p>
+              <p>PRICING</p>
+              <p>ADVERTISING</p>
+              <p>COMPANY</p>
+              <p>CAREERS</p>
+              <p>MY ACCOUNT</p>
+              <p>NEWSROOM/PRESS</p>
+            </div>
+          </div>
+          <div className="flex px-3 space-x-6">
+            <img src="/assets/Group 6422.png" alt="image" />
+            <img src="/assets/Group 6420.png" alt="image" />
           </div>
         </div>
       </div>
 
-      <div className="flex">
+      <div className="flex p-3 space-x-3">
         <img src="/assets/Group 6408.png" alt="image" />
         <img src="/assets/Group 6407.png" alt="image" />
         <img src="/assets/Group 6406.png" alt="image" />
@@ -30,21 +53,3 @@ function Sidebar() {
 }
 
 export default Sidebar;
-
-{
-  /* <div>
-          <img src="/assets/Group 6408.png" alt="image" />
-          <img src="/assets/Group 6407.png" alt="image" />
-        </div>
-        <div>
-          <img src="/assets/Group 6421.png" alt="image" />
-        </div>
-        <div>
-            <p></p>
-        </div>
-      </div>
-      <div className="flex">
-        <img src="/assets/Group 6409.png" alt="image" />
-        <img src="/assets/Group 6422.png" alt="image" />
-        <img src="/assets/Group 6420.png" alt="image" /> */
-}
